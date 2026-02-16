ff49b6a20106ee462ebaa0f36f6543cbd11d615b
Goshen Jimenez <goshenjimenez@gmail.com>
2026-02-16 19:09:35 +0800

    feat - Pager

--- GoshenJimenez.CSMWeb/Pages/Students/Index.cshtml.cs
+++ GoshenJimenez.CSMWeb/Pages/Students/Index.cshtml.cs
@@ -12,11 +12,18 @@ public class Index : PageModel
     private SchoolDbContext _dbContext;
     public List<GoshenJimenez.CSMWeb.Infrastructure.Models.Student> Students { get; set; }
     public string? Keyword { get; set; }
+    public int? PageIndex { get; set; }
+    public int? PageSize { get; set; }
+    public int? TotalPages { get; set; }
+    public int? TotalRecords { get; set; }
+    public string? SortBy { get; set; }
+    public string? SortOrder { get; set; }
+    
     public Index(SchoolDbContext dbContext){
         _dbContext = dbContext;
     }
 
-    public async Task OnGet(string? sortBy="fullname", string? sortOrder="asc", string? keyword="")
+    public async Task OnGet(int? pageIndex = 1, int? pageSize = 10, string? sortBy="fullname", string? sortOrder="asc", string? keyword="")
     {
         keyword = keyword?.ToLower() ?? "";
 
@@ -47,8 +54,18 @@ public class Index : PageModel
             query = query.OrderByDescending(s => s.Age);            
         }
 
+        var skip = ((pageIndex ?? 1) - 1) * (pageSize ?? 10);
+        var take = pageSize ?? 10;
+
+        query = query.Skip(skip).Take(take);
         Students = await query.ToListAsync();
-        
+        Keyword = keyword;
+        PageIndex = pageIndex;
+        PageSize = pageSize;
+        TotalRecords = await _dbContext.Students.CountAsync();
+        TotalPages = (int)Math.Ceiling(TotalRecords.Value / (double)(pageSize ?? 10));
+        SortBy = sortBy;
+        SortOrder = sortOrder;  
     }
 }
 
